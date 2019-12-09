10df9f2adeb5a1a0ea47657ff222899018f36c63
986281622 <986281622@qq.com>
2019-12-09 11:30:30 +0800

    知识点08

--- Pages/home/home.js
+++ Pages/home/home.js
@@ -5,62 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titles: ['张皮皮', '张米米', '许老二']
+  },
+  handleBtnClick(){
+    console.log("按钮点击")
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleTouchStart(){
+    console.log("handleTouchStart")
+  },
+  handleTouchMove(){
+    console.log("handleTouchMove")
+  },
+  handleTouchEnd(){
+    console.log("handleTouchEnd")
+  },
+  handleTap(){
+    console.log("handleTap")
+  },
+  handleLongPress(){
+    console.log("handleLongPress")
+  },
+  handleEventClick(event){
+    console.log("```````", event)
+  },
+  handleInter(event) {
+    console.log(event)
+  },
+  handleIntemClick(event) {
+    console.log(event)
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title, index)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //--------------事件冒泡和事件捕获
+  handleCaptureView1(){
+    console.log("handleCaptureView1")
+  },
+  handleBindView1(){
+    console.log("handleBindView1")
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleCaptureView2(){
+    console.log("handleCaptureView2")
+  },
+  handleBindView2(){
+    console.log("handleBindView2")
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleCaptureView3(){
+    console.log("handleCaptureView3")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBindView3(){
+    console.log("handleBindView3")
   }
+
 })
